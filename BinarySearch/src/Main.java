70154a387474f0e2f906fca00268e66181366f32
Batu Uzun <uzunbatu@hotmail.com>
2024-12-03 17:13:13 +0300

    initial commit

--- BinarySearch/src/Main.java
+++ BinarySearch/src/Main.java
@@ -4,7 +4,7 @@ public class Main {
 	public static void main(String[] args) {
 		int[] array = {1, 5, 7, 8, 15, 25, 35, 68};
 		
-		System.out.println(BinarySearch.binarySearch(array, 68));
+		System.out.println(BinarySearch.binarySearch(array, 68)); 
 	}
 
 }
